17b21fbaedd260544f34c62628b1aa4ed6881be0
neysa19 <neysaalfaro@gmail.com>
2024-11-05 02:22:13 -0600

    Episodio 10 - Model Factories

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model
+class Employer extends Model
 {
-    use HasFactory
+    /** @use HasFactory<\Database\Factories\EmployerFactory> */
+    use HasFactory;
 }

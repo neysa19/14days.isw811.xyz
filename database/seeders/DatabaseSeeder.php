ee65833376be909d738c7468a3ebfe9b28036e6f
neysa19 <neysaalfaro@gmail.com>
2024-11-07 03:49:41 -0600

    Episodio 15 - Understanding Database Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +18,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'Neysa',
+            'last_name' => 'Alfaro',
+            'email' => 'neysa@example.com',
         ]);
+        $this->call(JobSeeder::class);
     }
 }
